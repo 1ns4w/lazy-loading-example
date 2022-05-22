aeedc7ed8a54ae667462f89f017e5bd370f32434
ins4w <amejia.foo@gmail.com>
2022-05-22 15:43:22 -0500

    feat: add skeleton for images preview

--- main.js
+++ main.js
@@ -9,37 +9,39 @@ const getRandomFoxURL = async () => {
   }
 }
 
-const createImageNode = async () => {
+const createImageContainer = async () => {
+  const imageContainer = document.createElement('div')
+  imageContainer.classList.add('images__image-container')
   const image = document.createElement('img')
   image.classList.add('images__image')
   image.dataset.src = await getRandomFoxURL() // prop to hold url value til its rendered on intersection
-  return image
+  imageContainer.appendChild(image)
+  return imageContainer
 }
 
-const appendImageNode = async () => {
-  const imageNode = await createImageNode()
-  imagesContainer.appendChild(imageNode)
-  observer.observe(imageNode)
+const appendImage = async () => {
+  const imageContainer = await createImageContainer()
+  imagesContainer.appendChild(imageContainer)
+  observer.observe(imageContainer.firstChild)
 }
 
-const popImageNode = () => {
-  const imageNodes = document.querySelector('#images')
-  imagesContainer.removeChild(imageNodes.lastChild)
+const removeImages = () => {
+  imagesContainer.remove([...imagesContainer.childNodes])
 }
 
-const observer = new IntersectionObserver(async entries => {
+const observer = new IntersectionObserver(entries => {
   const [entry] = entries // intersection observer object
   if (entry.isIntersecting) {
-    const imageNode = entry.target
-    imageNode.src = imageNode.dataset.src
-    observer.unobserve(imageNode) // prevent further intersections observing
+    const image = entry.target
+    image.src = image.dataset.src
+    observer.unobserve(image) // prevent further intersections observing
   }
 })
 
 const imagesContainer = document.querySelector('#images')
 
 const addImageButton = document.querySelector('#addImage')
-addImageButton.addEventListener('click', appendImageNode)
+addImageButton.addEventListener('click', appendImage)
 
 const dumpImagesButton = document.querySelector('#dumpImages')
-dumpImagesButton.addEventListener('click', popImageNode)
+dumpImagesButton.addEventListener('click', removeImages)
